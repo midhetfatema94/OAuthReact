686dd54051b70e80ebcbffec61c53a5fa2267c7e
Midhet Sulemani <midhetfatema94@gmail.com>
2017-04-06 08:43:38 +0530

    minor changes, added text field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+
+    componentWillMount () {
+        firebase.initializeApp ({
+            apiKey: "AIzaSyC1n5i_3w2krdY8oFdcGfTucMjCAdM82eQ",
+            authDomain: "authreact-2d157.firebaseapp.com",
+            databaseURL: "https://authreact-2d157.firebaseio.com",
+            storageBucket: "authreact-2d157.appspot.com",
+            messagingSenderId: "265664894120"
+        });
+    }
 
     render () {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App!</Text>
+                <LoginForm />
             </View>
         );
     };
